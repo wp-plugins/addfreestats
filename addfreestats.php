233335e5a41f059af2193af7a05689059e9a8b0b
AddFreeStats <AddFreeStats@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-08 19:16:39 +0000

    tagging version 3.0
    git-svn-id: https://plugins.svn.wordpress.org/addfreestats/trunk@1040600 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- addfreestats.php
+++ addfreestats.php
@@ -3,18 +3,18 @@
 /*
 Plugin Name: AddFreeStats
 Plugin URI: http://www.addfreestats.com/
-Description: AddfreeStats Analytics provides to Website owner free website statistics on their visitors. Insert AddFreeStats asynchronous tracking code snippet into your blog with this plugin.
-Version: 2.0
+Description: AddFreeStats offers one of the best real-time website analytics tool using the latest technologies. Easy to use! Easy to install
+Version: 3.0
 Author: AddFreeStats
 Author URI: http://www.addfreestats.com/
 */
 
   class AddFreeStats_Plugin {
 
-         var $version = "2.0";
+         var $version = "3.0";
 
         function admin_menu() {
-                add_submenu_page('options-general.php', __('AddFreeStats Plugin'), __('AFSWP'), 5, __FILE__, array($this,'plugin_menu'));
+                add_submenu_page('options-general.php', __('AddFreeStats Plugin'), __('AFSWP'), 'manage_options', __FILE__, array($this,'plugin_menu'));
         }
 
         function wp_footer() {
@@ -27,7 +27,8 @@ Author URI: http://www.addfreestats.com/
 
                 // update options
 
-
+              if (!empty($_POST))
+              {
                 if ($_POST['afs_account'])
                 {
                 update_option('afs_account', $_POST['afs_account']);
@@ -59,7 +60,7 @@ Author URI: http://www.addfreestats.com/
                 
                 if ($afs_x!=0 || $afs_y !=0) //58
                 {
-                $afs_code .="<DIV ID=\"addfreestats-wordpress\" STYLE=\"position:relative;bottom:";
+                $afs_code .="<div id=\"addfreestats-wordpress\" STYLE=\"position:relative;bottom:";
                 $afs_code .="$afs_y";
                 
                    if ($afs_x!=0)
@@ -74,12 +75,22 @@ Author URI: http://www.addfreestats.com/
                    }
                              
                 }
-                else $afs_code .="<DIV ID=\"addfreestats-wordpress\" align=\"center\" STYLE=\"position:relative;bottom: 2px;\">\n";
+                else $afs_code .="<div id=\"addfreestats-wordpress\" align=\"center\" STYLE=\"position:relative;bottom: 2px;\">\n";
                 
-                    
-                
-               $afs_code .="<div id='addfreestats'></div>\n";
+              
+                               
+               //$afs_code .="<div id='addfreestats'></div>\n";
                $afs_code .="<script type=\"text/javascript\"><!--\n";
+               $afs_code .="afsid =document.getElementById(\"addfreestats\");\n";
+               $afs_code .="if (afsid==null) { \n";
+               $afs_code .="var afsdiv = document.createElement(\"div\");\n";
+               $afs_code .="afsdiv.id=\"addfreestats\";\n";
+               $afs_code .="var content='<div id=\"addfreestats\"></div>';\n";
+               $afs_code .="document.getElementById('addfreestats-wordpress').innerHTML = content;\n";
+               $afs_code .="}\n";
+
+                
+                
                $afs_code .="var AFS_Account=\"$afs_account\";\n";
                $afs_code .="var AFS_Tracker=\"auto\";\n";
                $afs_code .="var AFS_Server=\"www";
@@ -105,55 +116,98 @@ Author URI: http://www.addfreestats.com/
                 update_option('afs_code', $afs_code);
                 wp_cache_flush();
                 }
-
+              }
 ?>
 
 <?php if ($message) : ?>
 <div id="message" class="updated fade"><p><?php echo $message; ?></p></div>
 <?php endif; ?>
 <div id="dropmessage" class="updated" style="display:none;"></div>
-<div class="wrap">
-<h2><?php _e('AddFreeStats Options'); ?></h2>
-<p><?php _e('<a title="AddFreeStats WordPress" href="http://www.addfreestats.com/wordpress.html">AddFreeStats WordPress
-Help</a>') ?></p>
-<p><?php _e('Code appear in your footer') ?></p>
-
-
-<form name="dofollow" action="" method="post">
-<table>
-<tr>
-
-<th scope="row" style="text-align:right; vertical-align:top;"><?php _e('AFS Account:')?></td>
-<td><input type="text" size="20" name="afs_account"  value="<?php echo stripcslashes(get_option('afs_account')); ?>"</td></tr>
-<tr>
-
-<th scope="row" style="text-align:right; vertical-align:top;"><?php _e('Right Position:')?></td>
-<td><input type="text" size="20" name="afs_x"  value="<?php echo stripcslashes(get_option('afs_x')); ?>"</td></tr>
-<tr>
-
-<th scope="row" style="text-align:right; vertical-align:top;"><?php _e('Bottom Position:')?></td>
-<td><input type="text" size="20" name="afs_y"  value="<?php echo stripcslashes(get_option('afs_y')); ?>"</td></tr>
-
-
-</table>
-<p class="submit">
-<input type="hidden" name="action" value="update" />
-<input type="submit" name="Submit" value="<?php _e('Update Options')?> &raquo;" />
-</p>
-</form>
-</div>
+       <script type="text/javascript">
+           
+        function register()
+        {
 <?php
+        $sitename = get_bloginfo('name');
+        $siteurl = get_bloginfo('url');
+        $sitedes = get_bloginfo('description');
+        print "var sitename='$sitename';\n";
+        print "var siteurl='$siteurl';\n";
+        print "var sitedes= '$sitedes';\n";
+        print "var gourl='http://www.addfreestats.com/wpsignup.php?';\n";
+        print "gourl+='&sitename='+encodeURIComponent(sitename);\n";
+        print "gourl+='&siteurl='+encodeURIComponent(siteurl);\n";
+        print "gourl+='&sitedes='+encodeURIComponent(sitedes);\n";
+?>
+        window.open(gourl,"AddFreeStats Sign Up","width=450, height=500,toolbar=no, scrollbars=no, resizable=no, top=10,left=10");
+        } 
+        </script>
 
-        } // plugin_menu
+        <div class="wrap" style="width:400px; border: 1px solid #ddd;margin: 10px auto;padding: 15px">
+            <div style="text-align:center">
+<?php
+$plugdir=plugins_url();
+print "<img src='$plugdir/addfreestats/logo.png'></a>\n";
+?>
+           <br>
+                <h3>How to install AddFreeStats?</h3>
+            </div>
+            <div style="text-align:left; padding-left:20px">
+                1 - New Website? -> <a href="javascript:register()">Click here</a> to get a Website ID.<br>
+                2 - Type your Website ID into the Website ID field.<br>
+                3 - Click on 'Update Options' button.<br>
+                4 - AddFreeStats code will be inserted into the footer. 
+                <br><br>
+                If you want to display it in other place, just paste the line: <I>
+                &ltdiv id="addfreestats"&gt&lt/div&gt </I> 
+                where you want the button to appear into your html page.
+
+                
+            </div>
+<?php
+        $account = get_option('afs_account');
+        if (!empty($account)) {
+            print "<div style='text-align:center'>";
+            print "<br><a href='http://new.addfreestats.com/?usr=$account' target='_blank'>View your website stats</a>";
+            print "</div>";
+        }
+?>
+          <div style="text-align:center">
+                <h3>Plugin Options</h3>
+            </div>
+            <div style="text-align:center; padding-left:20px">
+                <form name="dofollow" action="" method="post">
+                    <table>
+                        <tr>
+                            <td scope="row" style="text-align:left; vertical-align:middle;"><?php _e('Website ID:') ?></td>
+                            <td><input type="text" size="20" placeholder="8 digits" name="afs_account"  value="<?php echo stripcslashes(get_option('afs_account')); ?>"</td></tr>
+                        <tr>
+                            <td scope="row" style="text-align:left; vertical-align:middle;"><?php _e('Right Position:') ?></td>
+                            <td><input type="text" size="20" name="afs_x"  value="<?php echo stripcslashes(get_option('afs_x')); ?>"</td></tr>
+                        <tr>
+                            <td scope="row" style="text-align:left; vertical-align:middle;"><?php _e('Bottom Position:&nbsp;') ?></td>
+                            <td><input type="text" size="20" name="afs_y"  value="<?php echo stripcslashes(get_option('afs_y')); ?>"</td></tr>
+                    </table>
+                    <div class="submit" style="text-align:center">
+                        <input type="hidden" name="action" value="update" />
+                        <input type="submit" name="Submit" style="cursor:pointer" value="<?php _e('Update Options') ?> &raquo;" />
+                    </div>
+                </form>
+                <p><a title="AddFreeStats WordPress Analytics" href="http://www.addfreestats.com" target="_blank">AddFreeStats WordPress Plugin</a></p>
+            </div>
+        </div>
+<?php
+    
+       } // plugin_menu
 
 } // Another_WordPress_Tracker_Plugin
 
 $_awtp_plugin = new AddFreeStats_Plugin();
 
-add_option("afs_account", null, __('AddFreeStats Plugin'), 'yes');
-add_option("afs_x", null, __('AddFreeStats Plugin'), 'yes');
-add_option("afs_y", null, __('AddFreeStats Plugin'), 'yes');
-add_option("afs_code", null, __('AddFreeStats Plugin'), 'yes');
+add_option("afs_account", null,'', 'yes');
+add_option("afs_x", null,'', 'yes');
+add_option("afs_y", null,'', 'yes');
+add_option("afs_code",null,'','yes');
 add_action('admin_menu', array($_awtp_plugin, 'admin_menu'));
 add_action('wp_footer', array($_awtp_plugin, 'wp_footer'));
 
--- readme.txt
+++ readme.txt
@@ -1,104 +1,82 @@
 === AddFreeStats Plugin ===
 Contributors: AddFreeStats
 Donate link: http://www.addfreestats.com
-Tags: AddFreeStats,analytics, addfreestats analytics, stats, statistics, blog stats, blog statistics, statcounter, counter, google, google analytics, stat, statistic, tracking, track, tracker
+Tags: AddFreeStats, wordpress, wordpress stats, analytics, stats, statistics, free statistics, website stats, website statistics, blog statistics, statcounter, counter, google, google analytics, stat, statistic, tracking, track, tracker
 Requires at least: 2.0
-Tested up to: 3.3
-Stable tag: 1.3.2
+Tested up to: 4.0.1
+Stable tag: 3.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
 
 
-AddFreetats Analytics provides free website statistics in real-time. This plugin insert AddFreeStats asynchronous code 
-into your WordPress blog.
+
+[AddFreeStats](http://www.addfreestats.com/) Analytics offers one of the best real-time website analytics tool using the latest technologies.And it's free! 
+
 
 == Description ==
 
-AddFreetats Analytic wordpress plugin
+Easy to install and use! Get amazing information about your website traffic! Analyze your visitor behavior and understand how to improve your website content. 
 
-AddFreetats Analytics provides to blog owners free statistics on their visitors. 
-Monitor human activity on your blog in real-time! Now blogger can keep track of how many people 
-visit their website, when they come, how they found their pages and much, much more...
+[AddFreeStats Analytics](http://www.addfreestats.com/) offers you two real-time reports continuously updated. The dashboard displays your website metrics into a single page. The “Right now” report allows you to monitor the current activity on your website.
 
-Boost the speed of your blog! 
+**Real-Time Analytics Reporting:**
+With LiveDisplay technology, AddFreeStats provides real-time reporting. Your website data are tracked immediately and the reports continuously updated. 
+
+**Easy to use:**
+With our innovative Dashboard, all website statistics are displayed into a single page. The toolbar is intuitive and offers user-friendly features such as zoom, scroll, menu, etc. 
+
+**Ultimate Performance:**
+AddFreeStats has been built to deliver ultimate performance. The latest technologies were used including HTML5, JQuery , AJAX and cloud system.
+
+**Boost the speed of your blog:** 
 AddFreeStats Analytics now provides asynchronous tracking code snippet. This technology will 
 dramatically improve your blog pages speed load time and tracking accuracy. 
 
-Select tracker design!
-AddFreeStats Analytics allows you to select the tracker design displayed on your website pages. Visible or not!
+**Why choose AddFreeStats Analytics?** 
 
-Free traffic for your blog!
-By including your blog in our free website directory, increase your blog traffic and pagerank.
+* More than 950000 websites selected AddFreeStats
+* Access to All your website statistics on a single page.
+* View all statistics about your website's visitors on a single page.
+* Real-time tracking and reporting.
+* AddFreeStats code uses asynchronous tracking technology.
+* Your website is protected: if our servers become slow or unresponsive the load time of your website's pages is not affected.
+* AddFreeStats offers a log-size of the latest 1000 unique visitors.
+* We respect your privacy, AddFreeStats doesn't sell or share your personal info, email and statistics about your website
+* Unlike others AddFreeStats does not use data collected for changing your search engine rankings.
 
-Why choose AddFreeStats Analytics? 
 
-- Asynchronous tracking technology boosts your website speed load time! 
-- AddFreeStats offers a log-size of the latest 1000 unique visitors. 
-- AddFreeStats analytics is compliant with the privacy laws. 
-- Unlike other AddFreeStats does not use data collected for changing your search engine rankings. 
-- Established in 1999, AddFreeStats Analytics has become a leader on the market. 
-- More than 900000 websites and blogs are tracked by AddFreeStats
+**Live demo**
+[Live Demo] (http://www.addfreestats.com/dashboard.php?demo=1)
 
-Latest Features: 
 
-- Log-size available for the latest 1000 visitors 
-- Display Google search engine rankings for search terms 
-- Entry page now is displayed. 
-- Outbound links and adsense tracking clicks improved 
-- Asynchronous tracking code released 
-- Mobile, Smartphone, Tablet, Game Consoles detection. 
-- Smartphone VS Computer report. 
-- Search engine referrer shows up like: S.E.->Keywords 
-- OS and browser databases updated. 
-- Visitors on Map. 
-- Check your Website statistics on your mobile. 
-
-You can let anyone view your blog stats , or protect your blog statistics by providing a password. 
 
 == Installation ==
 
-Installing should be a piece of cake and take fewer than five minutes.
+Installing should be a piece of cake and take fewer than 3 minutes.
 
-1. Upload `addfreestats.php` to your `/wp-content/plugins/` directory.
-1. Activate the plugin through the 'Plugins' menu in WordPress.
-1. Click on "Settings" link
-1. Click on AFSWP
-1. Enter your AddFreeStats Account eg: 00531724
-1. You can change the position of the button (default 0,0) in playing with the "relative position" :)
+1. Install AddFreeStats plugin either via the WordPress plugin directory, or by uploading the files to your `/wp-content/plugins/` directory.
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Click on "Settings" link and on AFSWP option.
+4. AFS plugin page is displayed. Click on "Get a website ID" link.
+5. Enter the website ID into the website ID field.
+6. Click on "Update option" button.
 
+
+
+If you need additional help? Contact us on facebook: https://www.facebook.com/AddFreeStats
 
 == Frequently Asked Questions ==
 
 = Where I can see a sample statistics report?
 
-http://www.addfreestats.com/cgi-bin/showuni3.cgi
+[Live Demo] (http://www.addfreestats.com/dashboard.php?demo=1)
 
-= How to get an AddFreeStats account?
-
-http://www.addfreestats.com/
 
 = Is it possible to get an invisible tracker?
 
 Yes, AddFreeStats allows you to select the button displayed on your blog pages. Invisible or visible: this is your choice! 
 and It's absolutely free! 
-
-= Features
-
-Entire profile of the latest 1000 visitors. 
-Google Adsense tracking exit clicks 
-All pages of your blog are tracked ! 
-History of your Blog statistics and traffic. 
-Unique visitors, visits, page views 
-Referrer, Search engine, Keywords ... 
-Unique Statistics Report for each page. 
-Statistics for search engines, referrers report. 
-View the countries, regions and cities of your blog visitors. 
-You'll see which pages are the most popular. 
-How much time your visitors spent on your blog? 
-View stats about the operating systems and browsers used. 
-View stats about the key words used by your visitors. 
-Daily stats estimate of visits. 
-Daily estimate referrers and search engines stats. 
-Statistics with percentages and graphics. 
 
 
 = How long before I start seeing stats? =
@@ -106,20 +84,24 @@ Statistics with percentages and graphics.
 immediatly, statistics reports are in real-time.
 
 
+
 == Screenshots ==
 
-1. Daily report.
+1. Welcome.
 
-2. Visitors tracking.
+2. Features.
 
-3. Visitors on Google Map 
+3. On your screen. 
 
-4  Search engine keywords
+4  Visitors listing.
 
-5  Most requested page
+5  Plugin
 
-6  AddFreeStats Plugin
+== Changelog ==
 
-7  AddFreeStats on your mobile
+= 3.0 =
+New UI
+Use the addfreestats div tag to display the code where you want. 
+Removed deprecated function calls.
 
 
